f000310f3692064276a6dae4ab49d070d1eb2b6e
BunoM1234 <101809271+BunoM1234@users.noreply.github.com>
2022-06-09 13:53:01 -0300

    PlayerMovement avanzado

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+    public int maxJumps;
+    public GameObject jugador;
+    public GameObject camara;
+
+    int hasJump;
+    Rigidbody rb;
+    float CamaraY = 2.818f;
+    float CamaraX = -3.51f;
+
+    void Start()
+    {
+        hasJump = maxJumps;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position -= new Vector3(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.Space) && hasJump > 0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            hasJump--;
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Respawn")
+        {
+            hasJump = maxJumps;
+        }
+    }
+}
