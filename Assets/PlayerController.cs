2b36cfac96504e7b8b23a4f836445f3a6646dd9f
BrunoM1234 <101809271+BunoM1234@users.noreply.github.com>
2022-06-12 09:51:28 -0300

    Cambio de idea

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,54 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed;
-    public float rotationSpeed;
-    public float jumpForce;
-    public int maxJumps;
-    public GameObject jugador;
-    public GameObject camara;
+    public Vector3 gravedad;
+    public Vector3 jumpSpeed;
 
-    int hasJump;
     Rigidbody rb;
-    float CamaraY = 2.818f;
-    float CamaraX = -3.51f;
 
-    void Start()
+    bool isGrounded = false;
+
+    void Awake()
     {
-        hasJump = maxJumps;
+        Physics.gravity = gravedad;
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.anyKeyDown && isGrounded)
         {
-            transform.Translate(movementSpeed, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(-movementSpeed, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position -= new Vector3(0, 0, movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(0, 0, movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.Space) && hasJump > 0)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
-            hasJump--;
+            rb.velocity = jumpSpeed;
+            isGrounded = false;
         }
     }
     void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.tag == "Respawn")
-        {
-            hasJump = maxJumps;
-        }
+        isGrounded = true;
     }
 }
